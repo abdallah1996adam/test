6767100bf087418db9b2e9dbdc85a758ba27be7e
abdallah1996adam <abdallahaadam@protonmail.com>
2021-10-03 06:26:28 +0200

    :tada:

--- CotonChat/server/src/server.js
+++ CotonChat/server/src/server.js
@@ -1,15 +1,43 @@
 require("dotenv").config();
 
 const express = require("express");
+const app = express();
+const http = require("http");
 const cors = require("cors");
 const morgan = require("morgan");
+const { Server } = require("socket.io");
 
-const server = express();
+app.use(cors());
+app.use(morgan("dev"));
+//Création d'un serveur http pour socket.io
+const server = http.createServer(app);
+//Configuration de socket.io pour accepter l'appel http du Front-End
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+//Connexion à la socket.io
+io.on("connection", (socket) => {
+  //   console.log("User is connected ", socket.id);
 
-server.use(cors());
-server.use(morgan("dev"));
-
+  //when the user join a chat
+  socket.on("join_chat", (data) => {
+    socket.join(data);
+    // console.log(`User with ID: ${socket.id} joined ${data}`);
+  });
+  //when the user send a message
+  socket.on("send_message", (data) => {
+    socket.to(data.chatId).emit("receive_message", data);
+    console.log(data);
+  });
+  //when the user disconecct
+  socket.on("Disconnected", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
 server.listen(process.env.SERVER_PORT, () => {
-  console.log(`server is up and running on port${process.env.SERVER_PORT}`);
+  console.log(`server is up and running on port ${process.env.SERVER_PORT}`);
 });
